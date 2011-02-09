29b2d95185bd96b3d6e2f78b78d2153fcf73cedf
Ling Li <lilingv@gmail.com>
2011-02-10 00:29:21 +0800

    增加显示个人信息和照片的函数

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -4,8 +4,9 @@ class UsersController extends AppController {
 
     var $name = 'Users';    
     var $scaffold;
-    var $uses = array('User', 'MailActivate');
- 
+    var $uses = array('User', 'Person');
+    var $components = array('CasAuth', 'Session', 'RequestHandler');
+
     /**
      * 主要功能已经由 Auth 组件提供
      */
@@ -19,12 +20,30 @@ class UsersController extends AppController {
     }
 
     /**
-     * 设定用户密码
+     * 显示用户个人资料
      */
-    function passwd() {
-        
+    function view() {
+        $this->set('title_for_layout', '个人信息');
+
+        $user = $this->CasAuth->user();
+        $filter = $this->Person->primaryKey.'='.$user['User']['username'];
+        $person = $this->Person->find('first', array('conditions'=>$filter));
+        $this->set('person', $person['Person']);
+    }
+
+    function photo() {
+        $this->autoRender = false;
+        $user = $this->CasAuth->user();
+        $filter = $this->Person->primaryKey.'='.$user['User']['username'];
+        $person = $this->Person->find('first', array('conditions'=>$filter));
+        $this->RequestHandler->respondAs('image/jpeg');
+        if (!empty($person['Person']['jpegphoto'])) {
+            echo $person['Person']['jpegphoto'];
+        } else {
+            $fp = fopen('img/no-img.jpg', 'rb');
+            fpassthru($fp);
+            fclose($fp);
+        }
     }
 
 }
-
-?>
